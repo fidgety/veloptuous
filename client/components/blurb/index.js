96933dc5108de9802ca783028d7e79595464d968
Matthew Beasley <matthew.beasley@gmail.com>
2016-10-20 22:27:15 +0100

    tidy up components

--- client/components/blurb/index.js
+++ client/components/blurb/index.js
@@ -4,15 +4,13 @@ import ColourBar from '../colourBar';
 
 require('./style.scss');
 
-export default React.createClass({
-    render() {
-        return (<div>
-            <div className="logo">
-                <Logo />
-                <ColourBar />
-            </div>
-            <div className="tag-line">A cycling route guide to help you make the most enjoyable rides in the Peak District.</div>
-            <a className="button start-route" href="/">Start a route</a>
-        </div>);
-    }
-});
+export default function () {
+    return (<div>
+        <div className="logo">
+            <Logo />
+            <ColourBar />
+        </div>
+        <div className="tag-line">A cycling route guide to help you make the most enjoyable rides in the Peak District.</div>
+        <a className="button start-route" href="/planner">Start a route</a>
+    </div>);
+}
--- client/components/colourBar/index.js
+++ client/components/colourBar/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 require('./style.scss');
 
 export default () => {
-    return <div className="colour-bar">
-        <div className="colour-bar__a"></div>
-        <div className="colour-bar__b"></div>
-        <div className="colour-bar__c"></div>
-        <div className="colour-bar__d"></div>
-        <div className="colour-bar__e"></div>
-    </div>;
+    return (<div className="colour-bar">
+        <div className="colour-bar__a" />
+        <div className="colour-bar__b" />
+        <div className="colour-bar__c" />
+        <div className="colour-bar__d" />
+        <div className="colour-bar__e" />
+    </div>);
 };
